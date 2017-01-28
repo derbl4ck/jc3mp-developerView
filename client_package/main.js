04ddc4efb474c9d6654fbe8e9fbaab4908017c33
Lukas Berwanger <lukas.berwanger@googlemail.com>
2017-01-28 23:11:02 +0100

    changed design and updated some stuff

--- client_package/main.js
+++ client_package/main.js
@@ -8,21 +8,21 @@
 
 'use strict';
 
-const ui = new WebUIWindow('developerview', 'package://jc3mp-developerview/ui/index.html', new Vector2(viewportSize.x, viewportSize.y));
+const ui = new WebUIWindow('developerview', 'package://jc3mp-developerview/ui/index.html', new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 ui.autoResize = true;
 
 jcmp.ui.AddEvent('developerview_GETupdateCore', () => {
   const posi = [];
   
-  posi[0] = localPlayer.GetRenderTransform(0.0).position.x;
-  posi[1] = localPlayer.GetRenderTransform(0.0).position.y;
-  posi[2] = localPlayer.GetRenderTransform(0.0).position.z;
-  posi[3] = localPlayer.rotation.x;
-  posi[4] = localPlayer.rotation.y;
-  posi[5] = localPlayer.rotation.z;
-  posi[6] = localPlayer.camera.rotation.x;
-  posi[7] = localPlayer.camera.rotation.y;
-  posi[8] = localPlayer.camera.rotation.z;
+  posi[0] = jcmp.localPlayer.position.x;
+  posi[1] = jcmp.localPlayer.position.y;
+  posi[2] = jcmp.localPlayer.position.z;
+  posi[3] = jcmp.localPlayer.rotation.x;
+  posi[4] = jcmp.localPlayer.rotation.y;
+  posi[5] = jcmp.localPlayer.rotation.z;
+  posi[6] = jcmp.localPlayer.camera.rotation.x;
+  posi[7] = jcmp.localPlayer.camera.rotation.y;
+  posi[8] = jcmp.localPlayer.camera.rotation.z;
   posi[9] = jcmp.players.length;
   jcmp.ui.CallEvent('developerview_updateCore', JSON.stringify(posi));
 });
